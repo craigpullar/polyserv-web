e1e081fd654340ff8edc44fdea6a216f988af8fb
Craig Pullar <craigmichaelpullar@gmail.com>
2018-10-14 12:19:29 -0400

    intro complete

--- src/containers/intro/container.js
+++ src/containers/intro/container.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
-import ExampleCard from "../../components/example-card";
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
 
 class Intro extends PureComponent {
   static propTypes = {
@@ -9,9 +10,22 @@ class Intro extends PureComponent {
 
   render() {
     return (
-      <div className={this.props.classes.container}>
-        <ExampleCard />
-      </div>
+      <Card className={this.props.classes.container}>
+        <Typography variant="h1" className={this.props.classes.title}>
+          Polyserv Api
+        </Typography>
+
+        <Typography variant="h5" className={this.props.classes.subTitle}>
+          We are an api exposing polygon map data. Our aim is to make it
+           as easy and quick as possible for companies and developers
+           to use polygon data in their projects.
+        </Typography>
+
+        <Typography variant="h5" className={this.props.classes.subTitle}>
+          We are currently not available to the public but feel free to checkout
+           our demo below or sign up to our mailing list for news.
+        </Typography>
+      </Card>
     );
   }
 }
--- src/containers/intro/styles.js
+++ src/containers/intro/styles.js
@@ -2,7 +2,18 @@ const styles = {
   container: {
     maxWidth: 1024,
     margin: "2rem auto",
-    display: "block"
+    display: "block",
+    padding: '3rem',
+    backgroundColor: 'white',
+    textAlign: "center"
+  },
+  title: {
+    marginBottom: "4rem",
+    color: "#16a085",
+    fontWeight: 500
+  },
+  subTitle: {
+    marginBottom: "1rem"
   }
 };
 
