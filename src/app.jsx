15f3c4d03291cf876b5d7a1cd92ef02c76022ef6
rahulramesh888 <rahul.ramesh888@gmail.com>
2018-10-14 16:51:10 -0400

    Start with demo map

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Intro from "./containers/intro";
+import DemoMap from "./containers/demo-map";
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
+      <>
         <CssBaseline>
-          <Intro />
+          <DemoMap />
         </CssBaseline>
-      </React.Fragment>
+      </>
     );
   }
 }
