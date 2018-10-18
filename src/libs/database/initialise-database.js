dc8415a7125641e0469749767af730e3efb3dc62
Craig Pullar <craigmichaelpullar@gmail.com>
2018-10-17 20:05:07 -0400

    added settings to firestore

--- src/libs/database/initialise-database.js
+++ src/libs/database/initialise-database.js
@@ -1,13 +1,17 @@
 import firebase from "firebase";
 import "firebase/firestore";
 
-const initialiseDatabase = () =>
-  firebase
+const initialiseDatabase = () => {
+  const firestore = firebase
     .initializeApp({
       apiKey: process.env.FIREBASE_API_KEY,
       authDomain: process.env.FIREBASE_AUTH_DOMAIN,
       projectId: process.env.FIREBASE_PROJECT_ID
     })
     .firestore();
+
+  firestore.settings({ timestampsInSnapshots: true });
+  return firestore;
+};
 
 export default initialiseDatabase;
