1139fcb9a367c6320eb22fbff5d8790c06009438
Craig Pullar <craigmichaelpullar@gmail.com>
2018-10-20 13:50:02 -0400

    minor refactor

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Intro from "./containers/intro";
 import Footer from "./containers/footer";
+
 class App extends Component {
   render() {
     return (
