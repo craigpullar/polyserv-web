a97848a4135499793ba4ad8af6748d9682553035
Craig Pullar <craigmichaelpullar@gmail.com>
2018-10-20 13:47:57 -0400

    added footer

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Intro from "./containers/intro";
-
+import Footer from "./containers/footer";
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <CssBaseline>
           <Intro />
+          <Footer />
         </CssBaseline>
       </React.Fragment>
     );
