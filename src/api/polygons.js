b68141cd6ff4bbee4a6c269b8208d87e748041b7
Robert Freethy <rfreethy@uwo.ca>
2018-10-21 01:24:23 -0400

    use limit

--- src/api/polygons.js
+++ src/api/polygons.js
@@ -2,12 +2,13 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/api";
 
-export const getPolygons = ({ bounds }) => {
+export const getPolygons = ({ bounds, limit = 50 }) => {
   return new Promise((resolve, reject) => {
     axios
       .get(`${API_URL}/polygons/region`, {
         params: {
-          bounds
+          bounds,
+          limit
         }
       })
       .then(resolve)
