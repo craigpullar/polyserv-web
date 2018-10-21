1cf087a521dd82ed98eaaf928e8f50a0ee9971fe
Robert Freethy <rfreethy@uwo.ca>
2018-10-21 00:01:23 -0400

    get polygons by bounds and render to map

--- src/components/polygon-map/component.js
+++ src/components/polygon-map/component.js
@@ -1,28 +1,43 @@
-import React, {Component} from "react";
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import React, { Component } from "react";
+import ReactMapboxGl, { GeoJSONLayer, Feature } from "react-mapbox-gl";
+import { getPolygons } from "../../api/polygons";
+
+import { POLYGON_STYLES } from "./styles";
 
 const Map = ReactMapboxGl({
-    accessToken: "pk.eyJ1IjoicG9seXNlcnYiLCJhIjoiY2puaWFjaWk0MG1rZDNxcGNpbXlpNHdvZSJ9._TdURRzc8nZw7Id3FtVO2g"
-  });
+  accessToken:
+    "pk.eyJ1IjoicG9seXNlcnYiLCJhIjoiY2puaWFjaWk0MG1rZDNxcGNpbXlpNHdvZSJ9._TdURRzc8nZw7Id3FtVO2g"
+});
+
+const TORONTO_LATLNG = [-79.384293, 43.653908];
 
 class PolygonMap extends Component {
-    render(){
-        return (
-        <Map
-            style="mapbox://styles/mapbox/streets-v9"
-            containerStyle={{
-              height: "100vh",
-              width: "100vw"
-            }}>
-              <Layer
-                type="symbol"
-                id="marker"
-                layout={{ "icon-image": "marker-15" }}>
-                <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
-              </Layer>
-          </Map>
-          )
-    }
+  state = { geojson: {} };
+  componentWillMount() {
+    getPolygons({ bounds: "13,56,-87,-94" }).then(response => {
+      const geojson = {
+        type: "FeatureCollection",
+        features: response.data
+      };
+      this.setState({ geojson });
+    });
+  }
+
+  render() {
+    console.log(this.state.geojson);
+    return (
+      <Map
+        style="mapbox://styles/mapbox/streets-v9"
+        center={TORONTO_LATLNG}
+        containerStyle={{
+          height: "90vh",
+          width: "90vw"
+        }}
+      >
+        <GeoJSONLayer data={this.state.geojson} {...POLYGON_STYLES} />
+      </Map>
+    );
+  }
 }
 
 export default PolygonMap;
