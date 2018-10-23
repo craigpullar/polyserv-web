3b5b2ffeead73b015c3a50cf96e07c206e1230c8
Robert Freethy <rfreethy@uwo.ca>
2018-10-22 20:35:31 -0400

    remove then and catch

--- src/api/polygons.js
+++ src/api/polygons.js
@@ -2,16 +2,10 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/api";
 
-export const getPolygons = ({ bounds, limit = 50 }) => {
-  return new Promise((resolve, reject) => {
-    axios
-      .get(`${API_URL}/polygons/region`, {
-        params: {
-          bounds,
-          limit
-        }
-      })
-      .then(resolve)
-      .catch(reject);
+export const getPolygons = ({ bounds, limit = 50 }) =>
+  axios.get(`${API_URL}/polygons/region`, {
+    params: {
+      bounds,
+      limit
+    }
   });
-};
